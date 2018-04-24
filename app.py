53439cd68e993e3d77c9ec7228d32482b071df23
cyril moreau <cyrilcoaster@hotmail.com>
2018-04-23 23:08:10 -0400

    First commit

--- app.py
+++ app.py
@@ -30,8 +30,9 @@ async def on_ready():
 @client.command()
 async def ping(*args):
 
-	await client.say(":ping_pong: BAM!")
-
+	await client.say(":ping_pong: pouf!")
+	await asyncio.sleep(3)
+	await client.say(":warning: This bot was created by **Habchy#6969**, it seems that you have not modified it yet. Go edit the file and try it out!")
 # After you have modified the code, feel free to delete the line above so it does not keep popping up everytime you initiate the ping commmand.
 	
 client.run("NDM4MTQ3NDYwMTY3MjM3NjMy.DcAaJg.31OKg8BukGc992dAiBUSfTlCiho")
