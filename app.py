189e2f500c93fb78c3e643e786a98628f3dc5111
cyril moreau <cyrilcoaster@hotmail.com>
2018-04-24 22:09:40 -0400

    First commit

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ client = Bot(description="Basic Bot by Habchy#1665", command_prefix="!", pm_help
 
 # This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
 # Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
-"""
+
+
 @client.event
 async def on_ready():
 	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
@@ -27,7 +28,7 @@ async def on_ready():
 	print('You are running BasicBot v2.1') #Do not change this. This will really help us support you, if you need support.
 	print('Created by Habchy#1665')
 	return await client.change_presence(game=discord.Game(name='PLAYING STATUS HERE')) #This is buggy, let us know if it doesn't work.
-"""
+
 # This is a basic example of a call and response command. You tell it do "this" and it does it.
 @client.command()
 async def ping(*args):
